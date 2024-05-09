6b1204be375f79daec3e453109ebbc201e331d40
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2024-05-09 10:38:34 +0200

    wip

--- src/app/consulting/page.jsx
+++ src/app/consulting/page.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function consulting() {
   return (
     <>
       <h1>consulting</h1>
+      <Link href="/consulting/innovation-og-skalering">Innovation og Skalering</Link>
+      <Link href="/consulting/forretningsudvikling">Forretningsudvikling</Link>
+      <Link href="/consulting/forandring">Forandring</Link>
+      <Link href="/consulting/dokumentation">Dokumentation</Link>
+      <Link href="/consulting/kommunikation">Kommunikation</Link>
+      <Link href="/consulting/kommunikation/underpunkt">under</Link>
     </>
   );
 }
