97b0a22bb27c3cfd85acef872a70dc3596ae0a13
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2024-05-17 14:39:15 +0200

    footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 import { Inter, Poppins } from "next/font/google";
 
 export const inter = Inter({
@@ -20,9 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${poppins.variable} text-ibsilver-600`}>
+      <body className={`${inter.variable} ${poppins.variable} text-ibsilver-600 flex flex-col min-h-screen`}>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
