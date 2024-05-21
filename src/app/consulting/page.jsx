597b63d14046951a3f564df9dcedd4c479322b32
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2024-05-21 09:21:42 +0200

    wip

--- src/app/consulting/page.jsx
+++ src/app/consulting/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import PageTagBreadcrumb from "@/components/ui/pageTagBreadcrumb";
 import SplitSection from "@/components/splitSection";
 import SplitSectionChild from "@/components/splitSectionChild";
-import { H1 } from "@/components/ui/headings";
+import { H1, H2 } from "@/components/ui/headings";
 
 export default async function consulting() {
   const headersList = {
@@ -21,13 +21,31 @@ export default async function consulting() {
   return (
     <>
       <SplitSection>
-        <SplitSectionChild>
+        <SplitSectionChild style={"bg-ibsilver-200"}>
           <PageTagBreadcrumb currentPage={"Consulting"} />
-
-          <H1>Consulting</H1>
+          <H1>Sammen løser vi dine udfordringer</H1>
           <p className="mt-2 mb-4">Vi hjælper med at udvikle og implementere en effektiv strategi, hvor vi identificerer jeres udfordringer, og sikrer en bæredygtig og fremadseende løsning.</p>
+          <p className="mt-2 mb-4">
+            Vores ekspertområder dækker alt fra udviklingsfasen af nye ideer og strategier, samt det værdiskabende kommunikative arbejde, som følger en ny implementeret strategi.
+          </p>
+        </SplitSectionChild>
+        <SplitSectionChild type="img"></SplitSectionChild>
+      </SplitSection>
+      <SplitSection>
+        <SplitSectionChild style={"bg-ibgreen-400"}>
+          <PageTagBreadcrumb currentPage={"Consulting"} />
+          <H2>Sådan kommer du i gang</H2>
+        </SplitSectionChild>
+        <SplitSectionChild>
+          <p>Udforsk vores forskellige ydelser nedenfor og find det, der bedst matcher dine behov.</p>
+          <p>Klik på ydelsen for at læse mere om hvad vi kan gøre for dig indenfor området.</p>
+          <p>Skriv til os via vores kontaktformular for at aftale, hvordan vi bedst løser dine udfordringer.</p>
+          <p>VI glæder os til at høre fra dig!</p>
         </SplitSectionChild>
       </SplitSection>
+      <section className="bg-ibsilver-500">
+        <H2 className={"text-ibsilver-100"}>Vores ekspertområder</H2>
+      </section>
       <h1>Consulting</h1>
       <div className="flex gap-2 flex-wrap">
         {cards
--- src/components/ui/headings.jsx
+++ src/components/ui/headings.jsx
@@ -1,13 +1,13 @@
 function H1({ children }) {
-  return <h1 className="font-bold text-ibsilver-600 text-[36px]">{children}</h1>;
+  return <h1 className="font-bold text-[50px]">{children}</h1>;
 }
 
-function H2({ children }) {
-  return <h2 className="font-bold text-ibsilver-600 text-[36px]">{children}</h2>;
+function H2({ children, className }) {
+  return <h2 className={`font-bold text-[36px] ${className}`}>{children}</h2>;
 }
 
 function H3({ children }) {
-  return <h2 className="font-bold text-ibsilver-600 text-[36px]">{children}</h2>;
+  return <h3 className="font-bold text-[36px]">{children}</h3>;
 }
 
 export { H1, H2, H3 };
