1f96c63af6ef945ba681660dbf513f6fee7ede88
Emil Kristensen <emil00y1@stud.kea.dk>
2024-05-22 14:47:27 +0200

    published

--- src/components/splitSectionChild.jsx
+++ src/components/splitSectionChild.jsx
@@ -1,3 +1,14 @@
 export default function SplitSectionChild({ type, children, className, img }) {
-  return <div className={(img ? " h-full w-full max-h-[calc(100vh-79px)] " : "pb-8 md:pb-12 pt-[25px] px-2.5 sm:px-4 md:px-6 lg:px-8 xl:px-10 md:overflow-y-auto ") + `${className}`}>{children}</div>;
+  return (
+    <div
+      className={
+        (img
+          ? " h-full w-full max-h-[calc(100vh-79px)] "
+          : "pb-8 md:pb-12 pt-[25px] md:pt-[40px] px-2.5 sm:px-4 md:px-6 lg:px-8 xl:px-10 md:overflow-y-auto ") +
+        `${className}`
+      }
+    >
+      {children}
+    </div>
+  );
 }
