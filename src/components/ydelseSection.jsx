f1c31653e5fc69a0ac7fe819c811335084cebcce
Emil Kristensen <emil00y1@stud.kea.dk>
2024-05-28 20:48:38 +0200

    updated ydelsessection

--- src/components/ydelseSection.jsx
+++ src/components/ydelseSection.jsx
@@ -7,7 +7,10 @@ import Icon from "./ui/icon";
 import Link from "next/link";
 
 export default async function YdelseSection({ parent, title }) {
-  const { data, error } = await supabase.from("ib-product-cards").select("*").eq("parent", parent);
+  const { data, error } = await supabase
+    .from("ib-product-cards")
+    .select("*")
+    .eq("parent", parent);
 
   if (error || !data || data.length === 0) {
     // Handle the error case (e.g., return a 404 page or a different component)
@@ -20,28 +23,35 @@ export default async function YdelseSection({ parent, title }) {
     <>
       <SplitSection>
         <SplitSectionChild className="bg-ibsilver-500 text-ibsilver-100">
-          {slugChildrenData.map((child) => (
-            <div key={child.icon} className="md:min-h-[calc(100vh-79px)]">
-              <H2>{child.title}</H2>
-              {child.content.map((content, index) => (
-                <P key={index}>{content.text}</P>
-              ))}
-              <Link href={`/consulting/${child.url}`}>
-                <Button variant="ghost" size="noPadding" hasArrow>
-                  Læs mere
-                </Button>
+          <div className="flex flex-col gap-8 md:gap-20">
+            {slugChildrenData.map((child) => (
+              <div key={child.icon}>
+                <H2>{child.title}</H2>
+                {child.content.map((content, index) => (
+                  <P key={index}>{content.text}</P>
+                ))}
+                <Link href={`/consulting/${child.url}`}>
+                  <Button variant="ghost" size="noPadding" hasArrow>
+                    Læs mere
+                  </Button>
+                </Link>
                 <div className="flex justify-center pb-7 pt-5">
                   <Icon large iconVersion={child.icon} />
                 </div>
-              </Link>
-            </div>
-          ))}
+              </div>
+            ))}
+          </div>
         </SplitSectionChild>
-        <SplitSectionChild sticky className="bg-ibgreen-400 flex items-center">
+        <SplitSectionChild
+          sticky
+          className="bg-ibgreen-400 flex items-center max-md:hidden"
+        >
           <h2 className="font-medium text-base md:text-3xl">
             {title}s-
             <br />
-            <span className="font-bold text-[32px]/[1.4] pb-4 sm:text-4xl md:text-6xl lg:text-8xl xl:text-8xl">ydelser</span>
+            <span className="font-bold text-[32px]/[1.4] pb-4 sm:text-4xl md:text-6xl lg:text-8xl xl:text-8xl">
+              ydelser
+            </span>
           </h2>
         </SplitSectionChild>
       </SplitSection>
