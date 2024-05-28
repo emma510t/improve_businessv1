d7c347c7a49576127daee35dc19847dff54533c7
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2024-05-28 12:26:10 +0200

    form error state updated

--- src/components/ui/formError.jsx
+++ src/components/ui/formError.jsx
@@ -2,16 +2,8 @@ import Image from "next/image";
 
 export default function FormError({ children }) {
   return (
-    <p
-      className="max-w-60 flex text-sm items-start gap-1 text-ibred-400 mt-2"
-      role="alert"
-    >
-      <Image
-        src="/img/svg-icons/warning.svg"
-        width={20}
-        height={20}
-        alt="advarsels symbol"
-      />
+    <p className="max-w-60 flex text-xs items-center gap-1 text-ibred-400 mt-2" role="alert">
+      <Image src="/img/svg-icons/warning.svg" width={20} height={20} alt="advarsels symbol" />
       {children}
     </p>
   );
