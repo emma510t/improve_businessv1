0f997d664ed8ad41280f5240bb52d4ff7674c795
Emil Kristensen <emil00y1@stud.kea.dk>
2024-05-30 11:44:04 +0200

    h4 to h3

--- src/components/ui/caseCards.jsx
+++ src/components/ui/caseCards.jsx
@@ -5,10 +5,21 @@ export default function CaseCards({ title, intro, img, slug }) {
   return (
     <article className="flex flex-col gap-4 max-w-[380px] group">
       <Link href={`/cases/${slug}`}>
-        <Image src={`/img/cases/${img}.webp`} alt={title} loading="lazy" width={380} height={180} className="aspect-[19/9] object-cover bg-ibsilver-400" />
+        <Image
+          src={`/img/cases/${img}.webp`}
+          alt={title}
+          loading="lazy"
+          width={380}
+          height={180}
+          className="aspect-[19/9] object-cover bg-ibsilver-400"
+        />
         <div className="pt-4 md:pt-5">
-          <h4 className="text-xl md:text-[26px]/[34px] font-semibold font-poppins text-ibsilver-600 group-hover:underline">{title}</h4>
-          <p className="line-clamp-3 text-base pt-1 md:pt-1.5 md:text-lg">{intro}</p>
+          <h3 className="text-xl md:text-[26px]/[34px] font-semibold font-poppins text-ibsilver-600 font-poppins group-hover:underline">
+            {title}
+          </h3>
+          <p className="line-clamp-3 text-base pt-1 md:pt-1.5 md:text-lg">
+            {intro}
+          </p>
         </div>
       </Link>
     </article>
